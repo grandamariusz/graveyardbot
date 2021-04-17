eb20a288ee4683788bf6f837fb9c160de1ebbf9a
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2021-04-17 20:42:19 +0200

    fix map listing page number and prevent unnecessary request
    this fixes the page number being incorrect when the beatmap count is a
    multiple of limit, as well as adding a check to not send a request if the
    count is 0.

--- src/graveyardbot.py
+++ src/graveyardbot.py
@@ -617,10 +617,14 @@ async def sub_menu(ctx, submenu_title, submenu_color, beatmap_status, beatmap_co
     # Iterates over a user's beatmaps in groups of 5
     page = 0
     limit = 5
-    page_total = math.floor(beatmap_count / limit + 1)
+    page_total = math.ceil(beatmap_count / limit) or 1
     while True:
         # Grab 5 maps from user sorted by latest updated
-        beatmap_list = requests.get(f'https://osu.ppy.sh/api/v2/users/{osu_user_id}/beatmapsets/{beatmap_status}?limit={limit}&offset={page * limit}', headers={'Authorization': f'Bearer {await return_token()}'}).json()
+        if page_total > 0:
+            beatmap_list = requests.get(f'https://osu.ppy.sh/api/v2/users/{osu_user_id}/beatmapsets/{beatmap_status}?limit={limit}&offset={page * limit}', headers={'Authorization': f'Bearer {await return_token()}'}).json()
+        else:
+            # prevent unnecessary request
+            beatmap_list = []
 
         # Create and populate embed with beatmaps
         e = discord.Embed(title=submenu_title, color=submenu_color)
